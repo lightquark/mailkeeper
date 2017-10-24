6bb7b758a09e50cb5777fffbe42dc37db1c74649
Pavlo Shcherbak <pscherbak@gmail.com>
2017-10-24 17:06:00 +0300

    Initial commit
    Initial commit

---
+++ src/main/java/org/lightquark/mailkeeper/MailkeeperApplication.java
@@ -0,0 +1,12 @@
+package org.lightquark.mailkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailkeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailkeeperApplication.class, args);
+	}
+}
---
+++ src/test/java/org/lightquark/mailkeeper/MailkeeperApplicationTests.java
@@ -0,0 +1,16 @@
+package org.lightquark.mailkeeper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MailkeeperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
